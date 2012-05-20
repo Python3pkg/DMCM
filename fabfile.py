45145aa10a5b14ec0bb95c73dc3869c2a42fdc84
Paul <ahernp@ahernp.com>
2012-05-20 12:41:01 +0100

    Change font size of code.

--- fabfile.py
+++ fabfile.py
@@ -1,35 +1,77 @@
 """
 Fabfile for ahernp.com.
 """
-from fabric.api import * 
+import os
+from fabric.api import env, local, lcd, cd, run, task, hosts, settings, abort
 from fabric.contrib.console import confirm
+from datetime import datetime
+from decorator import decorator
+
+TOP_LEVEL_PATH = os.path.dirname(os.path.realpath(__file__))
 
 env.hosts = ['web']
 
+@decorator
+def timer(func, *args, **kwargs):
+    """Wrapper which outputs how long a function took to run."""
+    start_time = datetime.now()
+    result = func(*args, **kwargs)
+    end_time = datetime.now()
+    duration = end_time - start_time
+    print('# {} ran for {} (started at {:%H:%M:%S}, ended at {:%H:%M:%S})'\
+          .format(func.__name__, duration, start_time, end_time))
+    return result
+
+@task
+@hosts('localhost')
+def setup():
+    """Setup development environment."""
+    with lcd(TOP_LEVEL_PATH):
+        local('git pull')
+        local('python manage.py syncdb')
+        local('python manage.py runserver')
+
+@task
+@timer
+@hosts('localhost')
 def test():
-    with settings(warn_only=True):
-        result = local('python manage.py test', capture=True)
+    """Test dmcm."""
+    with settings(warn_only=True), lcd(TOP_LEVEL_PATH):
+        result = local('python manage.py test dmcm', capture=True)
     if result.failed and not confirm("Tests failed. Continue anyway?"):
         abort("Aborting at user request.")
 
-def commit():
+@task
+@timer
+def deliver():
+    """Add and commit changes"""
     local("git add -p && git commit")
 
-def push():
+@task
+@timer        
+def prepare_deploy():
+    """Test, commit and push changes. """
+    test()
+    deliver()
     local("git push")
 
-                  
-def prepare_deploy():
-    test()
-    commit()
-    push()
-
+@task
+@timer
 def deploy():
+    """Deploy dmcm onto live server."""
     code_dir = '~/project'
     with settings(warn_only=True):
         if run("test -d %s" % code_dir).failed:
-          run("git clone git@github.com:ahernp/DMCM.git %s" % code_dir)
+            run("git clone git@github.com:ahernp/DMCM.git %s" % code_dir)
     with cd(code_dir):
         run("git pull")
         run("~/webapps/django/apache2/bin/restart")
 
+@task
+@timer
+@hosts('localhost')
+def manage(*args):
+    """Locally execute Django command."""
+    with settings(warn_only=True):
+        with lcd(TOP_LEVEL_PATH):
+            local('python manage.py %s' % (' '.join(args)))
