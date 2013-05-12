8b24bbbe17dca0f1c4788e1d245f3edebf3dd58b
Paul Ahern <ahernp@ahernp.com>
2013-05-12 16:32:35 +0100

    Added links to return from edit pages to site.

--- dmcm/edit/views.py
+++ dmcm/edit/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.decorators import login_required
-from django.core.urlresolvers import reverse_lazy
+from django.core.urlresolvers import reverse
 from django.utils.decorators import method_decorator
 from django.views.generic.edit import CreateView, UpdateView
 from dmcm.models import Page
@@ -10,19 +10,23 @@ class PageCreateView(CreateView):
     template_name = 'dmcm/edit/page_detail.html'
     model = Page
     form_class = PageForm
-    success_url = reverse_lazy('dmcm_edit_list_pages')
     
     @method_decorator(login_required)
     def dispatch(self, *args, **kwargs):
         return super(PageCreateView, self).dispatch(*args, **kwargs)
     
-
+    def get_success_url(self): 
+        return reverse('dmcm_page_detail',args=(self.object.slug,))
+    
+    
 class PageUpdateView(UpdateView):
     template_name = 'dmcm/edit/page_detail.html'
     model = Page
     form_class = PageForm
-    success_url = reverse_lazy('dmcm_edit_list_pages')
     
     @method_decorator(login_required)
     def dispatch(self, *args, **kwargs):
         return super(PageUpdateView, self).dispatch(*args, **kwargs)
+
+    def get_success_url(self):
+        return reverse('dmcm_page_detail', args=(self.object.slug,))
